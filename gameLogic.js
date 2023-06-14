9eec3add0f97c7c702df40bc1832fc8a91b421a6
erenova <erenova6@gmail.com>
2023-06-14 15:25:09 +0300

    updated game logic and ui

--- gameLogic.js
+++ gameLogic.js
@@ -1,164 +1,235 @@
-// DOM elements -- start
-const rockDom = document.querySelector('[data-move="rock"]');
-const paperDom = document.querySelector('[data-move="paper"]');
-const scissorsDom = document.querySelector('[data-move="scissors"]');
-const playerScoreDom = document.querySelector(".player-score");
-const computerScoreDom = document.querySelector(".computer-score");
-const winnerDom = document.querySelector(".winner");
+//! Game Settings
+const settings = {
+  playerScore: 0,
+  computerScore: 0,
+  gameScore: 3,
+  round: 0,
+  setPlayerScore() {
+    document.querySelector(
+      `.scoreboard-player-score`
+    ).innerText = `${settings.playerScore}`;
+  },
+  setComputerScore() {
+    document.querySelector(
+      `.scoreboard-computer-score`
+    ).innerText = `${settings.computerScore}`;
+  },
+  resetScores() {
+    settings.computerScore = 0;
+    settings.playerScore = 0;
+    document.querySelector(
+      `.scoreboard-computer-score`
+    ).innerText = `${settings.computerScore}`;
+    document.querySelector(
+      `.scoreboard-player-score`
+    ).innerText = `${settings.playerScore}`;
+    document.querySelector(
+      `.scoreboard-result`
+    ).innerText = `Make a move to start the game`;
+  },
+  setRoundText(text) {
+    document.querySelector(`.scoreboard-result`).innerText = `${text}`;
+  },
+  playerLastMove: ``,
+  computerLastMove: ``,
+  moves: ["rock", "paper", "scissors"],
+};
 
-// Dom elements -- end
-
-// Variables -- start
-
-let computerScore = 0;
-let playerScore = 0;
-let elementID;
-// variables -- end
-
-// assign the scores -- start
-
-playerScoreDom.textContent = playerScore;
-computerScoreDom.textContent = computerScore;
-
-// assing the scores -- end
-
-// EventListener -- start
-[rockDom, paperDom, scissorsDom].forEach((element) => {
-  element.addEventListener("click", (e) => {
-    elementID = e.target.dataset.move;
-
-    console.log(game(playRound(elementID, getComputerChoice())));
-  });
-});
-// EventListener -- end
-
-// Function area -- start
-function getComputerChoice() {
-  randomNum = Math.floor(Math.random() * 3) + 1;
-  let choice;
-  switch (randomNum) {
-    case 1:
-      choice = "rock";
-      break;
-    case 2:
-      choice = "paper";
-      break;
-    case 3:
-      choice = "scissors";
-      break;
-  }
-  return choice;
+function computerMove() {
+  let random = Math.floor(3 * Math.random());
+  return settings.moves[random];
 }
 
-function playRound(playerInput, computerInput) {
-  console.log(playerInput);
-  console.log(computerInput);
+//!Game Rules || function area
+function playRound(pMove, cMove) {
+  if (pMove === settings.moves[0] && cMove === settings.moves[1]) {
+    ++settings.computerScore;
 
-  let computerWonText = `Computer Won The Round!`;
-  let playerWonText = `Player Won The Round!`;
-  let rockWon = `Rock Beats scissors`;
-  let paperWon = `Paper Beats Rock`;
-  let scissorsWon = `Scissors Beats Paper`;
-  playerInput = playerInput.toLowerCase();
-  let possibleOutcomes = ["rock", "paper", "scissors"];
-  if (
-    playerInput === possibleOutcomes[0] &&
-    computerInput === possibleOutcomes[1]
-  ) {
-    return {
-      winner: "computer",
-      move: "paper",
-      output: `${computerWonText} ${paperWon}`,
-    };
-  } else if (
-    playerInput === possibleOutcomes[0] &&
-    computerInput === possibleOutcomes[2]
-  ) {
-    return {
-      winner: "player",
-      move: "rock",
-      output: `${playerWonText} ${rockWon}`,
-    };
-  } else if (
-    playerInput === possibleOutcomes[1] &&
-    computerInput === possibleOutcomes[0]
-  ) {
-    return {
-      winner: "player",
-      move: "paper",
-      output: `${playerWonText} ${paperWon}`,
-    };
-  } else if (
-    playerInput === possibleOutcomes[1] &&
-    computerInput === possibleOutcomes[2]
-  ) {
-    return {
-      winner: "computer",
-      move: "scissors",
-      output: `${computerWonText} ${scissorsWon}`,
-    };
-  } else if (
-    playerInput === possibleOutcomes[2] &&
-    computerInput === possibleOutcomes[0]
-  ) {
-    return {
-      winner: "computer",
-      move: "rock",
-      output: `${computerWonText} ${rockWon}`,
-    };
-  } else if (
-    playerInput === possibleOutcomes[2] &&
-    computerInput === possibleOutcomes[1]
-  ) {
-    return {
-      winner: "player",
-      move: "scissors",
-      output: `${playerWonText} ${scissorsWon}`,
-    };
+    settings.setPlayerScore();
+    settings.setComputerScore();
+    if (
+      settings.playerScore !== settings.gameScore ||
+      settings.computerScore !== settings.gameScore
+    ) {
+      settings.setRoundText("Computer Won the Round!");
+    }
+  } else if (pMove === settings.moves[0] && cMove === settings.moves[2]) {
+    ++settings.playerScore;
+
+    settings.setPlayerScore();
+    settings.setComputerScore();
+    if (
+      settings.playerScore !== settings.gameScore ||
+      settings.computerScore !== settings.gameScore
+    ) {
+      settings.setRoundText("Player Won the Round!");
+    }
+  } else if (pMove === settings.moves[1] && cMove === settings.moves[0]) {
+    ++settings.playerScore;
+
+    settings.setPlayerScore();
+    settings.setComputerScore();
+    if (
+      settings.playerScore !== settings.gameScore ||
+      settings.computerScore !== settings.gameScore
+    ) {
+      settings.setRoundText("Player Won the Round!");
+    }
+  } else if (pMove === settings.moves[1] && cMove === settings.moves[2]) {
+    ++settings.computerScore;
+
+    settings.setPlayerScore();
+    settings.setComputerScore();
+    if (
+      settings.playerScore !== settings.gameScore ||
+      settings.computerScore !== settings.gameScore
+    ) {
+      settings.setRoundText("Computer Won the Round!");
+    }
+  } else if (pMove === settings.moves[2] && cMove === settings.moves[0]) {
+    ++settings.computerScore;
+
+    settings.setPlayerScore();
+    settings.setComputerScore();
+    if (
+      settings.playerScore !== settings.gameScore ||
+      settings.computerScore !== settings.gameScore
+    ) {
+      settings.setRoundText("Computer Won the Round!");
+    }
+  } else if (pMove === settings.moves[2] && cMove === settings.moves[1]) {
+    ++settings.playerScore;
+
+    settings.setPlayerScore();
+    settings.setComputerScore();
+    if (
+      settings.playerScore !== settings.gameScore ||
+      settings.computerScore !== settings.gameScore
+    ) {
+      settings.setRoundText("Player Won the Round!");
+    }
   } else {
-    return { winner: "", move: playerInput, output: "It's a Tie" };
+    settings.setRoundText("Round is tie!");
   }
 }
 
-function game(round) {
-  if (round.winner === "player") {
-    winnerDom.textContent = round.output;
-    playerScore++;
-    playerScoreDom.textContent = playerScore;
-  } else if (round.winner === "computer") {
-    winnerDom.textContent = round.output;
-    computerScore++;
-    computerScoreDom.textContent = computerScore;
+function game() {
+  if (
+    settings.computerScore >= settings.gameScore ||
+    settings.playerScore >= settings.gameScore
+  ) {
+    if (settings.computerScore > settings.playerScore) {
+      settings.setRoundText(`!!Computer Won The Game!!`);
+      styling.computerBorder.classList.add("winner-glow");
+    } else {
+      styling.playerBorder.classList.add("winner-glow");
+      settings.setRoundText(`!!Player Won The Game!!`);
+    }
+    setTimeout(() => {
+      moveList.forEach((e) => {
+        if (e.moveName !== "reset") {
+          e.moveDOM.classList.add(`disabled`);
+          e.moveDOM.disabled = true;
+        }
+      });
+    }, 500);
   }
+}
 
-  if (round.winner === "") {
-    winnerDom.textContent = round.output;
+function AnimationControl() {
+  if (settings.round == 0) {
+    startAnimation(settings.playerLastMove, settings.computerLastMove);
+    settings.round++;
+  } else {
+    clearAnimation();
+
+    setTimeout(() => {
+      startAnimation(settings.playerLastMove, settings.computerLastMove);
+    }, 10);
+    settings.round++;
+  }
+}
+
+//! style area
+
+const animationObject = {
+  computerAnimation: document.querySelector(`.computer-animation`),
+  playerAnimation: document.querySelector(`.player-animation`),
+  computerHolder: document.querySelector(`.modal-computer`),
+  playerHolder: document.querySelector(`.modal-player`),
+};
+
+const styling = {
+  playerBorder: document.querySelector(`.scoreboard-player`),
+  computerBorder: document.querySelector(`.scoreboard-computer`),
+};
+function startAnimation(player, computer) {
+  animationObject.computerAnimation.src = `assets/${computer}.svg`;
+  animationObject.playerAnimation.src = `assets/${player}.svg`;
+  animationObject.playerAnimation.classList.add("player-animating");
+  animationObject.computerAnimation.classList.add("computer-animating");
+}
+function clearAnimation() {
+  animationObject.playerAnimation.classList.remove("player-animating");
+  animationObject.computerAnimation.classList.remove("computer-animating");
+}
+
+//! DOM area
+const moveList = [
+  {
+    moveDOM: document.querySelector(".action-rock"),
+    moveName: `rock`,
+  },
+  {
+    moveDOM: document.querySelector(".action-paper"),
+    moveName: `paper`,
+  },
+  {
+    moveDOM: document.querySelector(".action-scissors"),
+    moveName: `scissors`,
+  },
+  { moveDOM: document.querySelector(`.reset-button`), moveName: `reset` },
+];
+
+moveList.forEach((e) => {
+  if (e.moveName == "reset") {
+    e.moveDOM.addEventListener("click", () => {
+      clearAnimation();
+      settings.round = 0;
+      styling.playerBorder.classList.remove("winner-glow");
+      styling.computerBorder.classList.remove("winner-glow");
+      settings.resetScores();
+      e.moveDOM.disabled = true;
+      e.moveDOM.classList.add("disabled");
+      setTimeout(() => {
+        moveList.forEach((e) => {
+          e.moveDOM.disabled = false;
+          e.moveDOM.classList.remove("disabled");
+          if (e.moveName !== "reset") {
+            e.moveDOM.classList.remove(`disabled`);
+            e.moveDOM.disabled = false;
+          }
+        });
+      }, 500);
+    });
     return;
   }
+  e.moveDOM.addEventListener("click", () => {
+    moveList.forEach((e) => {
+      if (e.moveDOM.disabled === false) {
+        e.moveDOM.disabled = true;
+        e.moveDOM.classList.add("disabled");
+        setTimeout(() => {
+          e.moveDOM.disabled = false;
+          e.moveDOM.classList.remove("disabled");
+        }, 500);
+      }
+    });
 
-  if (computerScore === 5 || playerScore === 5) {
-    computerScoreDom.textContent = computerScore;
-    playerScoreDom.textContent = playerScore;
-    if (computerScore > playerScore) {
-      let difference = computerScore - playerScore;
-      let oldPlayerScore = playerScore;
-      let oldComputerScore = computerScore;
-      playerScore = 0;
-      computerScore = 0;
-      computerScoreDom.textContent = computerScore;
-      playerScoreDom.textContent = playerScore;
-      winnerDom.textContent = `Game Ended! Computer Won the game with a ${difference}-point margin! 
-      Player: ${oldPlayerScore} - Computer: ${oldComputerScore}`;
-    } else {
-      let difference = playerScore - computerScore;
-      let oldPlayerScore = playerScore;
-      let oldComputerScore = computerScore;
-      playerScore = 0;
-      computerScore = 0;
-      computerScoreDom.textContent = computerScore;
-      playerScoreDom.textContent = playerScore;
-      winnerDom.textContent = `Game Ended! Player Won the game with a ${difference}-point margin! 
-      Player: ${oldPlayerScore} - Computer: ${oldComputerScore}`;
-    }
-  }
-}
+    settings.playerLastMove = e.moveName;
+    settings.computerLastMove = computerMove();
+    playRound(settings.playerLastMove, settings.computerLastMove);
+    game();
+    AnimationControl();
+  });
+});
